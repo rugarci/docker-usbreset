1fdc7030e9c4f07affb112853a0963b8ec4592ec
rugarci <rrggcc@gmail.com>
2019-12-06 09:49:46 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-export REAL_DEV=$(basename $(readlink -f $1))
 
-export DEV_FOLDER=$(find /sys/devices -lname *$REAL_DEV)/../..
+if [ $(dirname $1) == "/dev" ]; then
 
-export BUS_NUM=$(echo `printf "%03d" $(cat $DEV_FOLDER/busnum)`)
+  export REAL_DEV=$(basename $(readlink -f $1))
+  export DEV_FOLDER=$(find /sys/devices -lname *$REAL_DEV)/../..
+  export BUS_NUM=$(echo `printf "%03d" $(cat $DEV_FOLDER/busnum)`)
+  export DEV_NUM=$(echo `printf "%03d" $(cat $DEV_FOLDER/devnum)`)
 
-export DEV_NUM=$(echo `printf "%03d" $(cat $DEV_FOLDER/devnum)`)
+  echo $1 resolved to $BUS_NUM:$DEV_NUM
 
-echo $1 resolved to $BUS_NUM:$DEV_NUM
+  export DEV_BUS_FOLDER=/dev/bus/usb/$BUS_NUM/$DEV_NUM
+else
+  export DEV_BUS_FOLDER=$1
+fi
 
-./usbreset /dev/bus/usb/$BUS_NUM/$DEVNUM
+echo Resetting $DEV_BUS_FOLDER
+./usbreset $DEV_BUS_FOLDER
+
