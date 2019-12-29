0d83f031ffdc30a24044464b71f00db9c12531c2
rugarci <rrggcc@gmail.com>
2019-12-29 11:29:31 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+if [[ ! -v $1 ]]; then
+    echo "Device to reset not passed"
+    exit 1
+fi
 
 if [ $(dirname $1) == "/dev" ]; then
 
-  export REAL_DEV=$(basename $(readlink -f $1))
-  export DEV_FOLDER=$(find /sys/devices -lname *$REAL_DEV)/../..
-  export BUS_NUM=$(echo `printf "%03d" $(cat $DEV_FOLDER/busnum)`)
-  export DEV_NUM=$(echo `printf "%03d" $(cat $DEV_FOLDER/devnum)`)
+  export LINK_DEV=$(readlink -f $1)
 
-  echo $1 resolved to $BUS_NUM:$DEV_NUM
+  if [ $(dirname $LINK_DEV) == "/dev" ]; then
+    export REAL_DEV=$(basename $LINK_DEV)
+    export DEV_FOLDER=$(find /sys/devices -lname *$REAL_DEV)/../..
+    echo $DEV_FOLDER
+    export BUS_NUM=$(echo `printf "%03d" $(cat $DEV_FOLDER/busnum)`)
+    export DEV_NUM=$(echo `printf "%03d" $(cat $DEV_FOLDER/devnum)`)
 
-  export DEV_BUS_FOLDER=/dev/bus/usb/$BUS_NUM/$DEV_NUM
+    echo $1 resolved to $BUS_NUM:$DEV_NUM
+
+    export DEV_BUS_FOLDER=/dev/bus/usb/$BUS_NUM/$DEV_NUM
+  else
+    export DEV_BUS_FOLDER=$LINK_DEV
+    echo $1 points to $LINK_DEV
+  fi
 else
   export DEV_BUS_FOLDER=$1
 fi
